7c43bda538cbb0082fe8295272c1d06d4a38c2e5
Ricardo Quesada <ricardoquesada@gmail.com>
2017-01-21 08:19:41 -0800

    directory refactoring

--- alexa/commodorehome.py
+++ alexa/commodorehome.py
@@ -10,6 +10,8 @@ import aniso8601
 from flask import Flask, json, render_template
 from flask_ask import Ask, request, session, question, statement
 
+import uniclient
+
 
 app = Flask(__name__)
 ask = Ask(app, "/")
@@ -23,42 +25,24 @@ def launch():
     return question(welcome_text).reprompt(help_text)
 
 
-@ask.intent('OneshotTideIntent',
-    mapping={'city': 'City', 'date': 'Date'},
-    convert={'date': 'date'},
-    default={'city': 'seattle', 'date': datetime.date.today })
-def one_shot_tide(city, date):
-    if city.lower() not in STATIONS:
-        return supported_cities()
-    return _make_tide_request(city, date)
-
-
-@ask.intent('DialogTideIntent',
-    mapping={'city': 'City', 'date': 'Date'},
-    convert={'date': 'date'})
-def dialog_tide(city, date):
-    if city is not None:
-        if city.lower() not in STATIONS:
-            return supported_cities()
-        if SESSION_DATE not in session.attributes:
-            session.attributes[SESSION_CITY] = city
-            return _dialog_date(city)
-        date = aniso8601.parse_date(session.attributes[SESSION_DATE])
-        return _make_tide_request(city, date)
-    elif date is not None:
-        if SESSION_CITY not in session.attributes:
-            session.attributes[SESSION_DATE] = date.isoformat()
-            return _dialog_city(date)
-        city = session.attributes[SESSION_CITY]
-        return _make_tide_request(city, date)
-    else:
-        return _dialog_no_slot()
+@ask.intent('CommodoreGianaIntent')
+def do_giana(state):
+    statement_text = render_template('do_giana')
+    return statement(statement_text).simple_card("Commodore Home", statement_text)
 
 
 @ask.intent('CommodoreAlarmIntent',
     mapping={'state': 'alarm_state'}
         )
 def do_alarm(state):
+    on = ('on', 'enable', 'enabled')
+    off = ('off', 'disable', 'disabled')
+
+    if state in on:
+        uniclient.send_packet_v2('10.0.0.15', 2, CommdoreHome.ALARM_ON)
+    else:
+        uniclient.send_packet_v2('10.0.0.15', 2, CommdoreHome.ALARM_OFF)
+
     statement_text = render_template('do_alarm', alarm_state=state)
     return statement(statement_text).simple_card("Commodore Home", statement_text)
 
@@ -67,12 +51,20 @@ def do_alarm(state):
     mapping={'percent': 'percent'}
     )
 def do_dimmer(percent):
+    on = ('on', 'enable', 'enabled')
+    off = ('off', 'disable', 'disabled')
+
+    if state in on:
+        uniclient.send_packet_v2('10.0.0.15', 2, CommdoreHome.ALARM_ON)
+    else:
+        uniclient.send_packet_v2('10.0.0.15', 2, CommdoreHome.ALARM_OFF)
     statement_text = render_template('do_dimmer', dimmer_value=percent)
     return statement(statement_text).simple_card("Commodore Home", statement_text)
 
 
 @ask.intent('CommodorePlayerIntent',
-    mapping={'number': 'song_number'}
+    mapping={'number': 'song_number'},
+    default={'number': '0'}
     )
 def do_player(number):
     statement_text = render_template('do_player', song_number=number)
---
+++ alexa/uniclient.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+from __future__ import division, unicode_literals, print_function
+import socket
+import sys
+import struct
+import os
+
+UDP_PORT = 6464
+
+DEBUG = 0
+
+# Commodore Home Commands
+
+class CommodoreHome:
+    _,              \
+    SONG_0,         \
+    SONG_1,         \
+    SONG_2,         \
+    SONG_3,         \
+    SONG_4,         \
+    SONG_5,         \
+    SONG_6,         \
+    SONG_7,         \
+    SONG_STOP,      \
+    SONG_PLAY,      \
+    SONG_PAUSE,     \
+    SONG_RESUME,    \
+    SONG_NEXT,      \
+    SONG_PREV,      \
+    DIMMER_0,       \
+    DIMMER_25,      \
+    DIMMER_75,      \
+    DIMMER_100,     \
+    ALARM_OFF,      \
+    ALARM_ON,       \
+    ALARM_TRIGGER,  \
+    _,              \
+    _,              \
+    _,              \
+    _,              \
+    _,              \
+    _,              \
+    _,              \
+    _,              \
+    _ = range(31)
+    
+
+def log(mesg):
+    if DEBUG is not 0:
+        print(mesg)
+
+def discover_devices(callback):
+    pass
+
+def send_packet_v2(ipaddress, port, joyvalue1, joyvalue2):
+
+    log("target IP/Port %s/%d" % (ipaddress, UDP_PORT))
+
+    joyvalue1 = int(joyvalue1)
+    joyvalue2 = int(joyvalue2)
+    port = int(port)
+
+    log("Sending to control port:%d  joy=%d joy=%d" % (port, joyvalue1, joyvalue2))
+
+    message = struct.pack("BBBB", 2, port, joyvalue1, joyvalue2)
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.sendto(message, (ipaddress, UDP_PORT))
+
+def send_packet_v3(ipaddress, port, joyvalue1, pot1x=0, pot1y=0):
+
+    log("target IP/Port %s/%d" % (ipaddress, UDP_PORT))
+
+    joyvalue1 = int(joyvalue1)
+    port = int(port)
+    pot1x = int(pot1x)
+    pot1y = int(pot1y)
+
+    print("Sending to control port:%d  joy=%d, potx=%d, poty=%d" % (port, joyvalue1, pot1x, pot1y))
+
+    message = struct.pack("BBBBBBBB", 3, port, joyvalue1, 0, pot1x, pot1y, 0, 0)
+    sock = socket.socket(socket.AF_INET, # Internet
+                         socket.SOCK_DGRAM) # UDP
+    sock.sendto(message, (ipaddress, UDP_PORT))
+
+def help():
+    print("%s v0.1 - A tool to test the UniJoystiCle\n" % os.path.basename(sys.argv[0]))
+    print("%s ip_address port joy_value potx_value poty_value")
+    print("Example:\n%s 192.168.4.1 0 255 0 0" % os.path.basename(sys.argv[0]))
+    sys.exit(-1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 4:
+        help()
+
+    args = sys.argv[1:]
+
+    for i in range(1,2):
+        send_packet_v3(*args)
+
+
