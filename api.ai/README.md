5471521f0f9f507f99bbfc21c3c5477ddcfc3f5d
Ricardo Quesada <ricardoquesada@gmail.com>
2017-01-29 12:25:24 -0800

    api.ai: server

---
+++ api.ai/README.md
@@ -0,0 +1,11 @@
+# Commodore Home
+
+for Google Home
+
+## requirements
+
+```sh
+$ pip install flask
+$ pip install future
+```
+
---
+++ api.ai/app.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def action_alarm_setting(req):
+    print('alarm')
+    return {'alarm': True}
+
+def action_dimmer_setting(req):
+    print('dimmer')
+    return {'dimmer': True}
+
+def action_joystick_movement(req):
+    print('joy')
+    return {'joystick': True}
+
+def action_play_music(req):
+    print('music')
+    return {'music': True}
+
+def processRequest(req):
+    # actions:
+    #  - alarm-setting
+    #  - dimmer-setting
+    #  - joystick-movement
+    #  - play-music
+    actions = {'alarm-setting': action_alarm_setting,
+            'dimmer-setting': action_dimmer_setting,
+            'joystick-movement': action_joystick_movement,
+            'play-music': action_play_music}
+    action =  req.get("result").get("action")
+    if action in actions:
+        return actions[action](req)
+    else:
+        print('invalid action: %s' % action)
+    return {}
+
+
+def makeYqlQuery(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
+
+
+def makeWebhookResult(data):
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today in " + location.get('city') + ": " + condition.get('text') + \
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "apiai-weather-webhook-sample"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
